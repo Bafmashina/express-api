5e420b3a6d2ea9360a6cbf088ef8fa86d07af818
Aleksandr Kuzmin <sana.ru98@gmail.com>
2024-03-21 09:45:26 +0300

    Unfolow update

--- controllers/follow-controller.js
+++ controllers/follow-controller.js
@@ -1,70 +1,74 @@
 const { prisma } = require("../prisma/prisma-client");
 
 const FollowController = {
-  followUser: async (req, res) => {
-    const { followingId } = req.body;
-    const userId = req.user.userId;
+    followUser: async (req, res) => {
+        const { followingId } = req.body;
+        const userId = req.user.userId;
 
-    if (followingId === userId) {
-      return res.status(500).json({ message: 'Вы не можете подписаться на самого себя' });
-    }
-
-    try {
-      const existingSubscription = await prisma.follows.findFirst({
-        where: {
-           AND: [
-             {
-               followerId: userId
-             },
-             {
-               followingId
-             }
-           ]
+        if(followingId === userId) {
+            return res.status(500).json({ error: "Нельзя подписаться на себя" })
         }
-       })
 
-      if (existingSubscription) {
-        return res.status(400).json({ message: 'Подписка уже существует' });
-      }
+        try {
+            const existingSubscription = await prisma.follows.findFirst({
+                where: {
+                    AND: [
+                        {followerId: userId},
+                        {followingId}
+                    ]
+                }
+            })
 
-      await prisma.follows.create({
-        data: {
-          follower: { connect: { id: userId } },
-          following: { connect: { id: followingId } },
-        },
-      });
+            if(existingSubscription) {
+                return res.status(400).json({ error: "Вы уже подписаны" })
+            }
 
-      res.status(201).json({ message: 'Подписка успешно создана' });
-    } catch (error) {
-      console.log('error', error)
-      res.status(500).json({ error: 'Ошибка сервера' });
+            await prisma.follows.create({
+                data: {
+                    follower: { connect: { id: userId } },
+                    following: { connect: { id: followingId } }
+                    
+                }
+            })
+
+            res.status(201).json({ error: "Вы подписались" })
+
+        } catch (error) {
+            console.error("Follow user Error", error)
+            return res.status(500).json({ error: "Internal Server Error" })
+        }
+    },
+    unfollowUser: async (req, res) => {
+        const { followingId } = req.body;
+        const userId = req.user.userId;
+
+        try {
+            const follows = await prisma.follows.findFirst({
+                where: {
+                    AND: [
+                        {followerId: userId},
+                        {followingId}
+                    ]
+                }
+            })
+
+
+            if(!follows) {
+                return res.status(404).json({ error: "Вы не подписаны" })
+            }
+
+            await prisma.follows.delete({
+                where: { id: follows.id }
+            })
+
+            res.status(201).json({ error: "Вы отписались" })
+
+        } catch (error) {
+            console.error("Unfollow user Error", error)
+            return res.status(500).json({ error: "Internal Server Error" })
+        }
+
     }
-  },
-  unfollowUser: async (req, res) => {
-    const { followingId } = req.body;
-    const userId = req.user.userId;
-
-    try {
-      const follows = await prisma.follows.findFirst({
-        where: {
-          AND: [{ followerId: userId }, { followingId: followingId }]
-        },
-      });
-
-      if (!follows) {
-        return res.status(404).json({ error: "Запись не найдена" });
-      }
-
-      await prisma.follows.delete({
-        where: { id: follows.id },
-      });
-
-      res.status(200).json({ message: 'Отписка успешно выполнена' });
-    } catch (error) {
-      console.log('error', error)
-      res.status(500).json({ error: 'Ошибка сервера' });
-    }
-  }
-};
+}
 
 module.exports = FollowController;
